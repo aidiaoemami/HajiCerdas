4d2d1b0b1711d1004b456ccef2d753df137ca583
aidiaoemami <aidiaoemami@gmail.com>
2019-10-22 14:23:51 +0700

    Initial commit

---
+++ app/src/main/java/com/aidiaoemami/hajicerdas/AplikasiActivity.java
@@ -0,0 +1,14 @@
+package com.aidiaoemami.hajicerdas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AplikasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aplikasi);
+    }
+}
